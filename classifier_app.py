827bc6fa7435e146b4de563cda9a99f4ef670f72
juliw9 <100039028+juliw9@users.noreply.github.com>
2024-05-18 19:48:43 +0200

    Update classifier_app.py

--- classifier_app.py
+++ classifier_app.py
@@ -3,11 +3,12 @@ from transformers import CamembertForSequenceClassification, CamembertTokenizer
 import torch
 import json
 import requests
+import os
 
 # Function to download file from Google Drive
-def download_file_from_google_drive(url, destination):
-    session = requests.Session()
-    response = session.get(url, stream=True)
+def download_file_from_google_drive(file_id, destination):
+    url = f"https://drive.google.com/uc?export=download&id={file_id}"
+    response = requests.get(url, stream=True)
     token = None
 
     for key, value in response.cookies.items():
@@ -16,34 +17,30 @@ def download_file_from_google_drive(url, destination):
             break
 
     if token:
-        params = {'id': '1HbAX8pMeSNxv_ZTSItoOIV0GWcWIatNI', 'confirm': token}
-        response = session.get(url, params=params, stream=True)
+        params = {'id': file_id, 'confirm': token}
+        response = requests.get(url, params=params, stream=True)
 
     with open(destination, 'wb') as f:
         for chunk in response.iter_content(32768):
             f.write(chunk)
 
-# Load the configuration
+# File paths
 config_path = "config.json"
-
-# Load the tokenizer
-tokenizer = CamembertTokenizer.from_pretrained('camembert-base')
-
-# Streamlit app
-st.title("French Sentence Difficulty Classifier")
+model_path = "model.safetensors"
+file_id = "1HbAX8pMeSNxv_ZTSItoOIV0GWcWIatNI"  # Your file ID from Google Drive
 
 # Check if model file exists, if not download it
-model_path = "model.safetensors"
 if not os.path.exists(model_path):
     st.write("Downloading model file...")
-    download_file_from_google_drive('https://drive.google.com/uc?export=download', model_path)
+    download_file_from_google_drive(file_id, model_path)
     st.write("Model file downloaded successfully!")
 
-# Load the configuration from the file
+# Load the configuration
 with open(config_path, 'r') as f:
     config = json.load(f)
 
-# Load the model
+# Load the tokenizer and model
+tokenizer = CamembertTokenizer.from_pretrained('camembert-base')
 model = CamembertForSequenceClassification.from_pretrained('camembert-base', state_dict=torch.load(model_path), config=config)
 
 # Define a function to predict difficulty
@@ -54,6 +51,9 @@ def predict_difficulty(sentence):
     logits = outputs.logits
     predicted_class = torch.argmax(logits, dim=1).item()
     return config['id2label'][str(predicted_class)]
+
+# Streamlit app
+st.title("French Sentence Difficulty Classifier")
 
 sentence = st.text_input("Enter a French sentence:")
 
