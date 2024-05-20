d2c4d9401c936b9b298821a6fb704f5a2f456020
juliw9 <100039028+juliw9@users.noreply.github.com>
2024-05-20 18:13:28 +0200

    Update classifier_app2.py

--- classifier_app2.py
+++ classifier_app2.py
@@ -3,7 +3,6 @@ from transformers import CamembertTokenizer, CamembertForSequenceClassification
 import torch
 import os
 import requests
-import time
 
 def download_file(url, output_path):
     response = requests.get(url, stream=True)
@@ -62,8 +61,7 @@ if st.button("Evaluate"):
         for i, url in enumerate(model_parts_urls):
             part_path = model_file_path + f".part{i}"
             print("Downloading part", i, "from URL:", url)  # Print the URL being downloaded
-            time.sleep(10)
-            download_file(url, part_path)
+            #download_file(url, part_path)
             print("Downloaded part", i)  # Print when each part is downloaded
 
         num_parts = len(model_parts_urls)
