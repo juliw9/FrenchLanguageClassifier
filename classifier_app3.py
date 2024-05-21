44975b1c11830b7cee8962a171fda79df3f01a84
juliw9 <100039028+juliw9@users.noreply.github.com>
2024-05-21 18:58:27 +0200

    Update classifier_app3.py

--- classifier_app3.py
+++ classifier_app3.py
@@ -62,13 +62,13 @@ def set_background(image_path):
             background-repeat: no-repeat;
         }}
         .result-text {{
-            color: black;
+            color: white;
             font-size: 2em;
             font-weight: bold;
         }}
         .description-text {{
             font-size: 1em;
-            color: black;
+            color: white;
             margin-top: 10px;
         }}
         </style>
@@ -78,12 +78,12 @@ def set_background(image_path):
 
 # Dictionary mapping levels to descriptions
 level_descriptions = {
-    "A1": "Can understand and use familiar everyday expressions and very basic phrases aimed at the satisfaction of needs of a concrete type. Can introduce him/herself and others and can ask and answer questions about personal details such as where he/she lives, people he/she knows and things he/she has. Can interact in a simple way provided the other person talks slowly and clearly and is prepared to help.",
-    "A2": "Can understand sentences and frequently used expressions related to areas of most immediate relevance (e.g. very basic personal and family information, shopping, local geography, employment). Can communicate in simple and routine tasks requiring a simple and direct exchange of information on familiar and routine matters. Can describe in simple terms aspects of his/her background, immediate environment, and matters in areas of immediate need.",
-    "B1": "Can understand the main points of clear standard input on familiar matters regularly encountered in work, school, leisure, etc. Can deal with most situations likely to arise whilst travelling in an area where the language is spoken. Can produce simple connected text on topics that are familiar or of personal interest. Can describe experiences and events, dreams, hopes, and ambitions and briefly give reasons and explanations for opinions and plans.",
-    "B2": "Can understand the main ideas of complex text on both concrete and abstract topics, including technical discussions in his/her field of specialization. Can interact with a degree of fluency and spontaneity that makes regular interaction with native speakers quite possible without strain for either party. Can produce clear, detailed text on a wide range of subjects and explain a viewpoint on a topical issue giving the advantages and disadvantages of various options.",
-    "C1": "Can understand a wide range of demanding, longer texts, and recognize implicit meaning. Can express him/herself fluently and spontaneously without much obvious searching for expressions. Can use language flexibly and effectively for social, academic, and professional purposes. Can produce clear, well-structured, detailed text on complex subjects, showing controlled use of organizational patterns, connectors, and cohesive devices.",
-    "C2": "Can understand with ease virtually everything heard or read. Can summarize information from different spoken and written sources, reconstructing arguments and accounts in a coherent presentation. Can express him/herself spontaneously, very fluently, and precisely, differentiating finer shades of meaning even in more complex situations."
+    "A1": "Your friend can understand and use familiar everyday expressions and very basic phrases aimed at the satisfaction of needs of a concrete type. They introduce themselves and others and can ask and answer questions about personal details such as where they live, people they knows and things they have. They can interact in a simple way provided the other person talks slowly and clearly and is prepared to help.",
+    "A2": "Your friend can understand sentences and frequently used expressions related to areas of most immediate relevance (e.g. very basic personal and family information, shopping, local geography, employment). They can communicate in simple and routine tasks requiring a simple and direct exchange of information on familiar and routine matters. They can describe in simple terms aspects of his/her background, immediate environment and matters in areas of immediate need.",
+    "B1": "Your friend can understand the main points of clear standard input on familiar matters regularly encountered in work, school, leisure, etc. They can deal with most situations likely to arise whilst travelling in an area where the language is spoken. They produce simple connected text on topics which are familiar or of personal interest. Can describe experiences and events, dreams, hopes & ambitions and briefly give reasons and explanations for opinions and plans.",
+    "B2": "Your friend can understand the main ideas of complex text on both concrete and abstract topics, including technical discussions in their field of specialisation. They can interact with a degree of fluency and spontaneity that makes regular interaction with native speakers quite possible without strain for either party. They produce clear, detailed text on a wide range of subjects and explain a viewpoint on a topical issue giving the advantages and disadvantages of various options.",
+    "C1": "Your friend can understand a wide range of demanding, longer texts, and recognise implicit meaning. They express themselves fluently and spontaneously without much obvious searching for expressions. They can use language flexibly and effectively for social, academic and professional purposes. They can produce clear, well-structured, detailed text on complex subjects, showing controlled use of organisational patterns, connectors and cohesive devices.",
+    "C2": "Your friend can understand with ease virtually everything heard or read. They can summarise information from different spoken and written sources, reconstructing arguments and accounts in a coherent presentation. They can express themselves spontaneously, very fluently and precisely, differentiating finer shades of meaning even in more complex situations."
 }
 
 # Streamlit interface
