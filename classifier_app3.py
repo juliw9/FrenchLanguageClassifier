696ab056484cd420cb8e2046c27e6fd90fd331d6
juliw9 <100039028+juliw9@users.noreply.github.com>
2024-05-21 18:17:11 +0200

    Update classifier_app3.py

--- classifier_app3.py
+++ classifier_app3.py
@@ -5,6 +5,11 @@ import os
 import requests
 import base64
 
+# Helper function to convert image to base64
+def get_image_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode()
+
 # Function to download files
 def download_file(url, output_path):
     response = requests.get(url, stream=True)
